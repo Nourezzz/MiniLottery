eef3ae7297d6c69636cbddc1649d974ab4eae927
Noure <101957571+Nourezzz@users.noreply.github.com>
2023-05-14 14:07:48 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,4 +9,21 @@ module.exports = {
   networks: {
     hardhat: {},
   },
+   networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+      forking: {
+        url: "https://rpc.ankr.com/bsc"
+      },
+      accounts: {
+        count: 250,
+      }
+    },
+    bsctest: {
+      url: "https://data-seed-prebsc-1-s2.binance.org:8545/"
+    },
+    mumb: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/hQe09bWwSPichl2HSenFSf-lty-_d_WF"
+    }
+  },
 };
